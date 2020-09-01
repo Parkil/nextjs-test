c92ee47ae6fa3863a6f7083d558587a7bac63d44
Parkil <alkain77@revhat.com>
2020-09-01 14:56:26 +0900

    Test

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,6 +1,53 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 
-export default (req, res) => {
-  res.statusCode = 200
-  res.json({ name: 'John Doe' })
+// api별 설정
+export const config = {
+    api: {
+        bodyParser: {
+            sizeLimit: '1mb',
+        },
+    },
+}
+
+function runMiddleware(req, res) {
+    console.log('middleware called')
+    return new Promise((resolve, reject) => {
+        /*
+        fn(req, res, (result) => {
+            if (result instanceof Error) {
+                return reject(result)
+            }
+            return resolve(result)
+        })*/
+
+        return resolve(req, res)
+    })
+}
+
+
+function getRandomInt(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min)) + min; //최댓값은 제외, 최솟값은 포함
+}
+
+
+// pages/api는 next에서 지정한 api호출 경로임
+export default async (req, res) => {
+
+
+    let randomInt = getRandomInt(1,100)
+    let result = await fetch('https://jsonplaceholder.typicode.com/todos/'+randomInt)
+    result = await result.json()
+
+    res.setPreviewData({ id : 'preview'})
+
+    res.statusCode = 200
+    res.json(result)
+
+    // Run the middleware
+    //await runMiddleware(req, res)
+    //res.statusCode = 200
+    //res.json({ name: 'John Doe' })
+    //res.redirect('/')
 }
--- pages/index.js
+++ pages/index.js
@@ -1,65 +1,94 @@
 import Head from 'next/head'
 import styles from '../styles/Home.module.css'
 
-export default function Home() {
-  return (
-    <div className={styles.container}>
-      <Head>
-        <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
+export default function Home({result}) {
+    console.log('function component called')
+    return (
+        <div className={styles.container}>
+            <Head>
+                <title>Create Next App</title>
+                <link rel="icon" href="/favicon.ico" />
+            </Head>
 
-      <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
+            <main className={styles.main}>
+                <h1 className={styles.title}>
+                    Welcome to <a href="https://nextjs.org">Next.js!</a>
+                </h1>
 
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
-        </p>
+                <p className={styles.description}>
+                    Get started by editing{' '}
+                    <code className={styles.code}>pages/index.js</code>
+                </p>
 
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
+                <div className={styles.grid}>
+                    <a href="https://nextjs.org/docs" className={styles.card}>
+                        <h3>Documentation &rarr;</h3>
+                        <p>Find in-depth information about Next.js features and API. --- {result.id}</p>
+                    </a>
 
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
+                    <a href="https://nextjs.org/learn" className={styles.card}>
+                        <h3>Learn &rarr;</h3>
+                        <p>Learn about Next.js in an interactive course with quizzes!</p>
+                    </a>
 
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className={styles.card}
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
+                    <a
+                        href="https://github.com/vercel/next.js/tree/master/examples"
+                        className={styles.card}
+                    >
+                        <h3>Examples &rarr;</h3>
+                        <p>Discover and deploy boilerplate example Next.js projects.</p>
+                    </a>
 
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
+                    <a
+                        href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
+                        className={styles.card}
+                    >
+                        <h3>Deploy &rarr;</h3>
+                        <p>
+                            Instantly deploy your Next.js site to a public URL with Vercel.
+                        </p>
+                    </a>
+                </div>
+            </main>
+
+            <footer className={styles.footer}>
+                <a
+                    href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                >
+                    Powered by{' '}
+                    <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
+                </a>
+            </footer>
         </div>
-      </main>
+    )
+}
 
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
-        </a>
-      </footer>
-    </div>
-  )
+function getRandomInt(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min)) + min; //최댓값은 제외, 최솟값은 포함
+}
+
+
+/*
+    next.js에서 pre-rendering을 하는 방법에는 2가지가 있다
+    1.static-generation : 빌드시 html을 생성해서 요청시마다 생성된 html을 반환
+    2.SSR : 요청시마다 html을 신규로 생성
+*/
+
+//static-generation을 이용하여 데이터를 가져오는 방식
+export const getStaticProps = async () => {
+    let randomInt = getRandomInt(1,100)
+    let result = await fetch('https://jsonplaceholder.typicode.com/todos/'+randomInt)
+    result = await result.json()
+
+    console.log('result : ',result)
+
+    return {
+        props: {
+            result : result
+        }
+    }
 }
