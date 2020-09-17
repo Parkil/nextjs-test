05c949e60f249c0174a03697433558282501d0ca
Parkil <alkain77@revhat.com>
2020-09-17 10:11:50 +0900

    pre-rendering 예제 추가

--- pages/ssrtest.js
+++ pages/ssrtest.js
@@ -1,5 +1,6 @@
 import Head from 'next/head'
 import styles from '../styles/Home.module.css'
+import { useRouter } from 'next/router'
 import React from 'react'
 
 export default function Home({result}) {
@@ -72,29 +73,11 @@ function getRandomInt(min, max) {
 }
 
 /*
-    동적 page에서만 설정가능
-    fallback  설정 : build시 생성되지 않은 페이지(=url)접근시 fallback기능을 이용하여 페이지생성시까지 대기할수 있는 기능을 제공
- */
-/*
-export const getStaticPaths = async () =>  {
-    return {
-        fallback : true
-    }
-}
+    pre-rendering 중 SSR을 이용하는 방식
+    request시마다 html을 생성한다, 데이터 변경시마다 지연없이 데이터가 변경되는점은 장점이지만 static-generation보다 속도가 느린점은 단점
 */
-
-/*
-    next.js에서 pre-rendering을 하는 방법에는 2가지가 있다
-    1.static-generation : 빌드시 html을 생성해서 요청시마다 생성된 html을 반환
-    2.SSR : 요청시마다 html을 신규로 생성
-*/
-
-/*
-    static-generation을 이용하여 데이터를 가져오는 방식
-    - build시 단 1번만 실행된다 -> 한번 build된 데이터가 변경되지 않는다
-    - yarn dev 로 실행시 F5를 누를때마다 실행되나 이는 테스트 환경이므로 착오하지 말 것
-*/
-export const getStaticProps = async (context) => {
+export const getServerSideProps = async (context) => {
+    console.log('getServerSideProps  called')
     let randomInt = getRandomInt(1,100)
     let result = await fetch('https://jsonplaceholder.typicode.com/todos/'+randomInt)
     result = await result.json()
@@ -102,7 +85,6 @@ export const getStaticProps = async (context) => {
     return {
         props: {
             result : result
-        },
-        revalidate: 1 //단위 : sec => build - start 후 설정된 초 간격으로 신규 request가 들어오면 re-pre-rendering수행
+        }
     }
 }
